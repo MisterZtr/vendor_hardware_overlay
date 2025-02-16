93db12461538d1ef7f8f9ac596420aa90496c88c
Andy CrossGate Yan <GeForce8800Ultra@gmail.com>
2025-02-16 21:53:42 +0800

    Add resources for Realme 14 Pro+
    Change-Id: I3341fc144af639d0f8df53b5054b52685378a27d

--- overlay.mk
+++ overlay.mk
@@ -199,6 +199,8 @@ PRODUCT_PACKAGES += \
 	treble-overlay-razer-cheryl \
 	treble-overlay-realme-10pro \
 	treble-overlay-realme-10pro-systemui \
+	treble-overlay-realme-14proplus \
+	treble-overlay-realme-14proplus-systemui \
 	treble-overlay-realme-6 \
 	treble-overlay-realme-6i \
 	treble-overlay-realme-8-5g \
